884404c2b9d80da6e9160ce98befe943445cc2a0
Paul <paul.shuker@outlook.com>
2024-12-10 18:05:32 +0000

    benchmark text added

---
+++ lua/tests/benchmark_text.lua
@@ -0,0 +1,50 @@
+M = {}
+
+---@class Text
+local text = require("lua.textangle.text")
+
+local ITERATIONS = 100000
+local INPUT = {
+   "Lots of words!    And such ",
+   " sdsd ",
+   "fsdhfdv",
+   "22@#@!%FDFDF",
+   "  fsfs sfssdsd   sddd\tdfdfd",
+}
+
+local function benchmark_unravel_lines()
+   local start_time = os.clock()
+   for _ = 1, ITERATIONS do
+      text.unravel_lines(INPUT)
+   end
+   local time_taken = os.clock() - start_time
+   assert(time_taken < 1)
+end
+
+local function benchmark_format()
+   local line_width = 10
+   local hyphenates = { true, false }
+   local hyphenate_minimum_gaps = { 0, 1, 2, 20 }
+   local hyphenate_overflows = { true, false }
+
+   -- Run with various options.
+   for _, hyphenate in ipairs(hyphenates) do
+      for _, hyphenate_minimum_gap in ipairs(hyphenate_minimum_gaps) do
+         for _, hyphenate_overflow in ipairs(hyphenate_overflows) do
+            local start_time = os.clock()
+            for _ = 1, ITERATIONS do
+               text.format(INPUT, line_width, hyphenate, hyphenate_minimum_gap, hyphenate_overflow)
+            end
+            local time_taken = os.clock() - start_time
+            assert(time_taken < 1)
+         end
+      end
+   end
+end
+
+function M.run()
+   benchmark_unravel_lines()
+   benchmark_format()
+end
+
+return M
--- lua/tests/main.lua
+++ lua/tests/main.lua
@@ -1,11 +1,20 @@
 -- Call to run all tests.
 local TESTS = { "test_checker", "test_text" }
+local BENCHMARKS = { "benchmark_text" }
+
+print("Running " .. #BENCHMARKS .. " Benchmark(s)")
+
+for _, benchmark in ipairs(BENCHMARKS) do
+   local script = require("lua.tests." .. benchmark)
+   script.run()
+   print("  " .. benchmark .. "... PASSED")
+end
 
 print("Running " .. #TESTS .. " Test(s)")
 
-for _, test in pairs(TESTS) do
+for _, test in ipairs(TESTS) do
    local script = require("lua.tests." .. test)
    -- Every test class must have a run function.
    script.run()
-   print("  " .. test .. ": PASSED")
+   print("  " .. test .. "... PASSED")
 end
